cb32c0edcf446e9cf15870ae3a61437e1eec5b1b
Your Name <peterduytran1992@gmail.com>
2019-03-20 17:40:18 -0700

    2nd commit

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'Peter Tran',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'March 19, 2019'
-    },
-    {
-        'author' : 'Jane Paeng',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'March 20, 2019'
-    }    
-]
-
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
